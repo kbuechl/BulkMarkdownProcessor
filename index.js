028a871bfc8f58442d0634ea85d0e1be4bb01940
Karl Buechler <karlrbuechler@gmail.com>
2018-06-29 23:53:54 -0600

    added basic logic so processor is recursive through any sub folders

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-const fs = require("fs"),
+const fs = require('fs'),
   path = require("path"),
   mdProcessor = require("./mdProcessor");
+
+  const  { lstatSync, readdirSync } = require('fs');
+
+  const processDir = (currentLocation) =>{
+    //process files in current folder
+    processFilesInDir(currentLocation);
+    // get other directies
+   const directories = getDirectories(currentLocation);
+    //recursion if there are directories left
+   
+    if(directories){
+      directories.forEach(d=> processDir(d));
+    }
+   
+  };
+  
+  const processFilesInDir = (dirToProcess) =>{
+    fs.readdir(dirToProcess, (err, files) => {
+      files.forEach(file => {
+        if (path.extname(file) == ".md") {
+          mdProcessor.convert(dirToProcess, file);
+        }
+      });
+    });
+  };
+  
+const getDirectories = source => readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
+const isDirectory = source => lstatSync(source).isDirectory();
 
 let directoryToSearch = process.argv[2];
 
 if (!directoryToSearch) throw new Error("No directory provided in args");
 
-fs.readdir(directoryToSearch, (err, files) => {
-  files.forEach(file => {
-    if (path.extname(file) == ".md") {
-      mdProcessor.convert(directoryToSearch + "\\" + file);
-    }
-  });
-});
+console.log("started")
+processDir(directoryToSearch);
+console.log("Done")
--- mdProcessor.js
+++ mdProcessor.js
@@ -3,14 +3,20 @@ const fs = require("fs"),
   showdown = require("showdown"),
   converter = new showdown.Converter();
 
-exports.convert = filePath => {
+exports.convert = (p, file) => {
+  let filePath = path.join(p, file);
   if (path.extname(filePath) != ".md") throw new Error("This program only accepts markdown files");
 
   let markDown = fs.readFileSync(filePath, { encoding: "utf8" });
   let html = converter.makeHtml(markDown);
 
   let fileName = path.basename(filePath, ".md");
-  fs.writeFileSync("./html/" + fileName + ".html", html);
+  let htmlFileLocation = path.join(p, "html");
+
+if (!fs.existsSync(htmlFileLocation)){
+    fs.mkdirSync(htmlFileLocation);
+}
+  fs.writeFileSync(path.join(htmlFileLocation, fileName + ".html"), html);
 
   console.log(fileName + ".html written successfully");
 };
